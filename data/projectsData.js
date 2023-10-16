a53316d64e968f50b714c1a568c2bb7dd94421d2
Joseph <jvp.2703@gmail.com>
2023-10-16 12:58:34 +0100

    test

--- data/projectsData.js
+++ data/projectsData.js
@@ -3,18 +3,16 @@ export const projectsData = [
     title: "project1",
     id: 1,
     image:
-    "https://mosew.com/wp-content/uploads/2023/08/desktop-wallpaper-full-dark-black-screen-black-page.jpg",
+    "/mock/mock-wallpaper.jpg",
     description:
       "Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet  consectetur adipisicing elit",
   },
-
-  // "/mock/mock-wallpaper.jpg",
 
   {
     title: "project2",
     id: 2,
     image:
-      "https://mosew.com/wp-content/uploads/2023/08/desktop-wallpaper-full-dark-black-screen-black-page.jpg",
+      "/mock/mock-wallpaper.jpg",
     description:
       "Lorem ipsum dolor sit aLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet met consectetur adipisicing elit",
   },
@@ -22,14 +20,14 @@ export const projectsData = [
     title: "project3",
     id: 3,
     image:
-      "https://mosew.com/wp-content/uploads/2023/08/desktop-wallpaper-full-dark-black-screen-black-page.jpg",
+      "/mock/mock-wallpaper.jpg",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
   },
   {
     title: "project4",
     id: 5,
     image:
-      "https://mosew.com/wp-content/uploads/2023/08/desktop-wallpaper-full-dark-black-screen-black-page.jpg",
+      "/mock/mock-wallpaper.jpg",
     description:
       "Lorem iit amet consectetur adipisicingpsum dolor sit amet consectetur adipisicing elit",
   },
@@ -37,7 +35,7 @@ export const projectsData = [
     title: "project4",
     id: 6,
     image:
-      "https://mosew.com/wp-content/uploads/2023/08/desktop-wallpaper-full-dark-black-screen-black-page.jpg",
+      "/mock/mock-wallpaper.jpg",
     description:
       "Lorem ipsum dolor it ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet  consectetur adipisicing sit amet consectetur adipisicing elit",
   },
@@ -45,7 +43,7 @@ export const projectsData = [
     title: "project4",
     id: 7,
     image:
-      "https://mosew.com/wp-content/uploads/2023/08/desktop-wallpaper-full-dark-black-screen-black-page.jpg",
+      "/mock/mock-wallpaper.jpg",
     description:
       "LorLorem ipsum dolor sit amet it amet consectetur adipisicing elit",
   },
