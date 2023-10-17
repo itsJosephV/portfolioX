370e4cc67dc5a392e736f0bf30986dd1fab7fc0f
Joseph <jvp.2703@gmail.com>
2023-10-17 21:25:20 +0100

    test commit

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useRef } from "react";
-import Form from "../../components/form/Form";
+import Form from "../../components/_form/Form";
 import { Divider } from "@nextui-org/react";
 
 const Contact = () => {
